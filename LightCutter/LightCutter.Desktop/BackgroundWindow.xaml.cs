f1ed073b6b168527ba20d3b7cd5b8bea0267339b
surviveplus <shin-ichi@surviveplus.net>
2017-11-07 23:41:51 +0900

    #9 Add background window and Notify Icon

---
+++ LightCutter/LightCutter.Desktop/BackgroundWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Net.Surviveplus.LightCutter.Desktop
+{
+    /// <summary>
+    /// BackgroundWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class BackgroundWindow : Window
+    {
+        #region constructors
+
+        public BackgroundWindow()
+        {
+            InitializeComponent();
+            this.ShowActionPannel();
+        }
+
+        #endregion
+
+        #region methods
+
+        private MainWindow main = null;
+        
+        public void ShowActionPannel()
+        {
+            if (this.main == null)
+            {
+                this.main = new MainWindow();
+                this.main.Closed += (sender, e) => { this.main = null; };
+            }
+            this.main?.Show();
+        }
+
+        #endregion
+
+        #region Window events
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.main?.Close();
+        }
+
+        #endregion
+
+        #region Notify menu events
+
+        private void OpenMainAction_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShowActionPannel();
+        }
+
+        private void CloseAction_Click(object sender, RoutedEventArgs e)
+        {
+            this.Notify.Visibility = Visibility.Hidden;
+            this.Close();
+        }
+
+        #endregion
+
+    } // end class
+} // end namespace
