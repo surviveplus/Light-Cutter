4b971a364aa320781a60d4549634c05e18eca23e
surviveplus <shinichi.koga@avanade.com>
2018-03-19 17:39:10 +0900

    #12 create C# sample (Like VB OnStartupNextInstance)

---
+++ prototype/SingleInstanceApplication/SingleInstanceApplication/ApplicationEvents.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.Devices
+
+Namespace My
+    ' 次のイベントは MyApplication に対して利用できます:
+    ' Startup:アプリケーションが開始されたとき、スタートアップ フォームが作成される前に発生します。
+    ' Shutdown:アプリケーション フォームがすべて閉じられた後に発生します。このイベントは、アプリケーションが異常終了したときには発生しません。
+    ' UnhandledException:ハンドルされない例外がアプリケーションで発生したときに発生します。
+    ' StartupNextInstance:単一インスタンス アプリケーションが起動され、それが既にアクティブであるときに発生します。 
+    ' NetworkAvailabilityChanged:ネットワーク接続が接続されたとき、または切断されたときに発生します。
+    Partial Friend Class MyApplication
+        Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+            Form1.Label1.Text = "MyApplication_Startup :" & String.Join(", ", e.CommandLine)
+
+        End Sub
+
+        Private Sub MyApplication_Shutdown(sender As Object, e As EventArgs) Handles Me.Shutdown
+
+        End Sub
+
+        Private Sub MyApplication_UnhandledException(sender As Object, e As UnhandledExceptionEventArgs) Handles Me.UnhandledException
+
+        End Sub
+
+        Private Sub MyApplication_StartupNextInstance(sender As Object, e As StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
+            Form1.Label1.Text = "MyApplication_StartupNextInstance :" & String.Join(", ", e.CommandLine)
+
+        End Sub
+
+        Private Sub MyApplication_NetworkAvailabilityChanged(sender As Object, e As NetworkAvailableEventArgs) Handles Me.NetworkAvailabilityChanged
+
+        End Sub
+    End Class
+End Namespace
---
+++ prototype/SingleInstanceApplication/SingleInstanceApplicationCSharpWpf/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingleInstanceApplicationCSharpWpf
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.MainWindow = new MainWindow();
+            this.MainWindow.Show();
+            (this.MainWindow as MainWindow).ShowSomething("OnStartup : " + string.Join(",", e.Args) );
+        }
+
+        public void OnStartupNextInstance(Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs e)
+        {
+            this.MainWindow.WindowState = WindowState.Normal;
+            this.MainWindow.Activate();
+
+            (this.MainWindow as MainWindow).ShowSomething("OnStartupNextInstance : " + string.Join(",", e.CommandLine));
+        }
+    }
+}
