cfe39d3df1862627fa89ee3fc9e578b8ba7affc0
Shinichi Koga (Avanade) <shinichi.koga@avanade.com>
2019-06-07 16:53:38 +0900

    Create new OCR Sample.

---
+++ prototype/OcrSample/OcrSample/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OcrSample
+{
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void DoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            try
+            {
+                var dialog = new OpenFileDialog();
+                if( dialog.ShowDialog(this).Value )
+                {
+
+                    var text = new System.Text.StringBuilder();
+                    button.IsEnabled = false;
+                    {
+                        var result = await OcrWrapper.RecognizeAsync(dialog.FileName);
+                        foreach (var line in result.Lines)
+                        {
+                            text.AppendLine(line.Text);
+                        }
+                    }
+                    this.resultBlock.Text = text.ToString();
+                } // end if
+
+            }
+            finally
+            {
+                button.IsEnabled = true;
+            }
+
+
+        }
+    }
+
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+}
---
+++ prototype/OcrSample/OcrSample/OcrWrapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage.Streams;
+
+namespace OcrSample
+{
+    public static class OcrWrapper
+    {
+
+        public static async Task<OcrResult> RecognizeAsync(string path) {
+            OcrResult result;
+            using (var bitmap = await LoadImage(path).ConfigureAwait(false))
+            {
+                result = await RecognizeAsync(bitmap).ConfigureAwait(false); 
+            }
+            return result;
+        }
+
+        public static async Task<OcrResult> RecognizeAsync(SoftwareBitmap bitmap)
+        {
+            //var ocrEngine = OcrEngine.TryCreateFromLanguage(new Windows.Globalization.Language("en-US"));   // This code does not work in my computer, English is not installed ?
+            //Debug.WriteLine(OcrEngine.IsLanguageSupported(new Windows.Globalization.Language("en"))); // false
+
+            var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+            Debug.WriteLine(ocrEngine.RecognizerLanguage.DisplayName);
+            var ocrResult = await ocrEngine.RecognizeAsync(bitmap);
+            return ocrResult;
+        } // end function
+
+
+        private static async Task<SoftwareBitmap> LoadImage(string path)
+        {
+            var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(path);
+            using (var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream);
+                var bitmap = await decoder.GetSoftwareBitmapAsync();
+                return bitmap;
+            }
+        }
+
+    } // end class
+
+
+    public static class TaskExtensions
+    {
+        public static Task<T> AsTask<T>(this IAsyncOperation<T> operation)
+        {
+            if (operation == null) throw new ArgumentNullException(nameof(operation));
+            var source = new TaskCompletionSource<T>();
+            operation.Completed = delegate 
+            {
+                switch (operation.Status)  
+                {
+                    case AsyncStatus.Completed: source.SetResult(operation.GetResults()); break;
+                    case AsyncStatus.Error: source.SetException(operation.ErrorCode); break;
+                    case AsyncStatus.Canceled: source.SetCanceled(); break;
+                }
+            };
+            return source.Task;
+        }
+        public static TaskAwaiter<T> GetAwaiter<T>(this IAsyncOperation<T> operation)
+        {
+            return operation.AsTask().GetAwaiter();
+        }
+    }
+} // end namespace
